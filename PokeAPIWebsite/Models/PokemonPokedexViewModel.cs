d14fa6b003b931f087380a0806f798e8c39aca4a
Spencer Sterling <spencer.sterling2001@gmail.com>
2020-10-28 07:43:34 -0700

    Pokedex ViewModel

---
+++ PokeAPIWebsite/Models/PokemonPokedexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PokeAPIWebsite.Models
+{
+    public class PokemonPokedexViewModel
+    {
+        /// <summary>
+        /// Infomation for a single Pokedex entry
+        /// </summary>
+        public string Nanme { get; set; }
+        public int Id { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public IEnumerable<string> MoveList { get; set; }
+    }
+}
